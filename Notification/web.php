29749dc20ba401544793cd1048e29fc5e7d53d49
letsbegin010101 <169005088+letsbegin010101@users.noreply.github.com>
2024-05-06 01:34:02 +0100

    Delete Notification directory

--- Notification/web.php
+++
@@ -1,6 +0,0 @@
-Route::group(['middleware' => 'samlauth'], function() {
-Route::get('/notifications', 'UserNotificationController@show');
-Route::post('/notificationsRead/{id}', 'UserNotificationController@markSingleAsRead');
-Route::post('/notificationsReadAll', 'UserNotificationController@markAllAsRead');
-
-}
